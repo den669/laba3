b643dcb1bfb76bf8ac43ea66d33a6b2bfb8acd06
den669 <71308629+den669@users.noreply.github.com>
2023-04-04 00:42:55 +0300

    Add files via upload

---
+++ laba4/builder/builder/Class2.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace builder
+{
+        public abstract class Pizza
+        {
+            public abstract Pizza Copy();
+        }
+
+        public class Pizzza : Pizza
+        {
+            public string crust, sauce, toppings;
+            public string Data() => crust + "\n" + sauce + "\n" + toppings + "\n";
+            public void GetCrust(string dat) => crust = dat;
+            public void GetSauce(string dat) => sauce = dat;
+            public void GetToppings(string dat) => toppings = dat;
+
+            public override Pizza Copy()
+            {
+
+                Pizzza clone = (Pizzza)this.MemberwiseClone();
+                clone.crust = String.Copy(crust);
+                clone.sauce = String.Copy(sauce);
+                clone.toppings = String.Copy(toppings);
+                return clone;
+            }
+
+        }
+
+        public interface Builder
+        {
+            void BuildCrust();
+
+            void BuildSauce();
+
+            void BuildToppings();
+            Pizzza GetPizza();
+        }
+        public class NormalPizzzaBuilder : Builder
+        {
+            private Pizzza plane;
+
+            public NormalPizzzaBuilder()
+            {
+                plane = new Pizzza();
+            }
+
+            public void BuildCrust()
+            {
+                plane.GetCrust("Add Crust");
+            }
+
+            public void BuildSauce()
+            {
+                plane.GetSauce("Add Sauce");
+            }
+
+            public void BuildToppings()
+            {
+                plane.GetToppings("Add Toppings");
+            }
+
+            public Pizzza GetPizza() => plane;
+        }
+        public class AwesomePizzzaBuilder : Builder
+        {
+            private Pizzza piz;
+
+            public AwesomePizzzaBuilder()
+            {
+                piz = new Pizzza();
+            }
+            public void BuildCrust()
+            {
+                piz.GetCrust("Add  awesome Crust");
+            }
+            public void BuildSauce()
+            {
+                piz.GetSauce("Add awesome Saucet");
+            }
+
+            public void BuildToppings()
+            {
+                piz.GetToppings("Add  awesome Toppings");
+            }
+
+            public Pizzza GetPizza() => piz;
+
+        }
+        public class Director
+        {
+            private Builder builder;
+
+            public Director(Builder builder)
+            {
+                this.builder = builder;
+
+            }
+            public void ChosePizzza(Builder builder)
+            {
+                this.builder = builder;
+            }
+
+            public Pizzza Buildpiz()
+            {
+                builder.BuildCrust();
+                builder.BuildSauce();
+                builder.BuildToppings();
+                return builder.GetPizza();
+            }
+
+        }
+
+
+    }
+
---
+++ laba4/builder/builder/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace builder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Builder pizza = new AwesomePizzzaBuilder();
+            Director director = new Director(pizza);
+            Pizzza pizz = director.Buildpiz();
+            richTextBox1.Text += pizz.Data();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Builder pizza2 = new NormalPizzzaBuilder();
+            Director director = new Director(pizza2);
+            Pizzza pizz2 = director.Buildpiz();
+            richTextBox1.Text += pizz2.Data();
+            for (int i = 0; i < Convert.ToInt32(textBox1.Text); i++)
+            {
+                Pizza clone = pizz2.Copy();
+                richTextBox1.Text += "Копія" + (i + 1) + ((Pizzza)clone).crust + ((Pizzza)clone).sauce + ((Pizzza)clone).toppings + "\n";
+            }
+        }
+    }
+}
